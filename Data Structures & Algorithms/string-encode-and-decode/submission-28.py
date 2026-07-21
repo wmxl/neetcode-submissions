d3f3901667d5fb51b221f68edee612ce61407536
Tony <294036748@qq.com>
2026-07-21 13:07:24 +0800

    Add: string-encode-and-decode - submission-28

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-28.py
@@ -0,0 +1,35 @@
+class Solution:
+    sep = '_'
+
+    def encode(self, strs: List[str]) -> str:
+        parts = []
+        for s in strs:
+            new_s = s.replace('/', '//')
+            new_s1 = new_s.replace(self.sep, '/' + self.sep)
+            parts.append(new_s1)
+            parts.append(self.sep)
+        st = "".join(parts)
+        return st
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        word = ""
+        n = len(s)
+        i = 0
+        while i < n:
+            if s[i] == '/' and i + 1 < n:
+                if s[i+1] == self.sep or s[i+1] == '/':
+                    word += s[i+1]
+                    i += 2
+                    continue
+
+            elif s[i] == self.sep:
+                res.append(word)
+                word = ""
+                i += 1
+                continue
+
+            word += s[i]
+            i += 1
+
+        return res
