00d25b8d25e83a050bdc74662ebd87c7d6e80ce7
Tony <294036748@qq.com>
2026-07-21 10:07:09 +0800

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,25 @@
+class Solution:
+    sep = chr(1) 
+    def encode(self, strs: List[str]) -> str:
+        
+        parts = []
+        for s in strs:
+            parts.append(s)
+            parts.append(self.sep)
+        return "".join(parts)
+
+
+    def decode(self, s: str) -> List[str]:
+        
+        res = []
+        word = ""
+        for c in s:
+            if c == self.sep:
+                res.append(word)
+                word = ""
+                continue
+            word += c
+        return res
+
+
+
