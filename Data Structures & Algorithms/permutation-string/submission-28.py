0c3badf75025c5c4a2b4f2549c39e41115465e1f
Tony <294036748@qq.com>
2026-08-04 10:51:58 +0800

    Add: permutation-string - submission-28

---
+++ Data Structures & Algorithms/permutation-string/submission-28.py
@@ -0,0 +1,44 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        n1 = len(s1)
+        n2 = len(s2)
+        if n1 > n2:
+            return False
+
+        def c2n(c):
+            return ord(c) - ord('a')
+
+        d = [0] * 26
+        d2 = [0] * 26
+
+        matched = 26
+
+        for c in s1:
+            d[c2n(c)] += 1
+            if d[c2n(c)] == 1:
+                matched -= 1
+
+        i = j = 0
+        # print(f'd:{d}')
+        while j < n2:
+            c = s2[j]
+            d2[c2n(c)] += 1 
+            if d2[c2n(c)] == d[c2n(c)]:
+                    matched += 1
+            if d2[c2n(c)] - 1 == d[c2n(c)]:
+                matched -= 1
+            # print(f'c:{c} mached:{matched} d2:{d2}')
+            if matched == 26:
+                return True
+
+            if j - n1 + 1 >= 0:
+                c2 = s2[j - n1 + 1]
+                d2[c2n(c2)] -= 1
+                if d2[c2n(c2)] == d[c2n(c2)]:
+                    matched += 1
+                if d2[c2n(c2)] + 1 == d[c2n(c2)]:
+                    matched -= 1
+
+            j += 1     
+        return False
+             
